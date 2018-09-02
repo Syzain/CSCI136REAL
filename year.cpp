f9784a12b3e64e3eaed226281c4ed013b98b2d21
Syzain <shussain6399@gmail.com>
2018-09-02 19:44:10 -0400

    Update year.cpp

--- year.cpp
+++ year.cpp
@@ -4,11 +4,23 @@ Course: CSCI-136
 Instructor: their name
 Assignment: Lab1A Task B
 
-This program takes 3 numbers and outputs the smallest integer.
+This program checks if the year inputted is a leap year
 */
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main(){
-		
+    int year;
+    
+    cout << "Enter a year: ";
+    cin >> year;
+    
+    if (year % 4 != 0)
+        cout << year << " is a common year.";
+    else if (year % 100 != 0)
+        cout << year << " is a leap year.";
+    else if (year % 400 !=0)
+        cout << year << " is a common year.";
+    else
+        cout << year << " is a leap year.";
+    return 0;
 }
